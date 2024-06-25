a8d731c474098fa1300e1f497e762c13c76b9e94
lucilebgt <lucile.bergeat@oclock.school>
2024-06-25 05:41:15 -0400

    navigation bug fixed

--- App.js
+++ App.js
@@ -2,20 +2,23 @@
 import React from 'react';
 import About from './components/about';
 import Search from './components/search';
-import { TabNavigator } from 'react-navigation';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-const Tab = TabNavigator({
-  Search: { screen: Search },
-  About: { screen: About }
+const Stack = createNativeStackNavigator();
 
-})
+const App = () => {
 
-export default function App() {
+
+
   return (
-    <>
-      <Tab />
-
-    </>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Search" component={Search} />
+        <Stack.Screen name="About" component={About} />
+      </Stack.Navigator>
+    </NavigationContainer>
   )
 }
+export default App;
 
--- metro.config.js
+++ metro.config.js
@@ -5,3 +5,4 @@ const { getDefaultConfig } = require('expo/metro-config');
 const config = getDefaultConfig(__dirname);
 
 module.exports = config;
+
