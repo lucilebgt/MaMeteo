7197038e4ff4ea728b1716656ff1bfbd41fd68d9
lucilebgt <lucile.bergeat@oclock.school>
2024-06-25 03:34:35 -0400

    maj env dev

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 import { registerRootComponent } from 'expo';
+import { AppRegistry } from 'react-native';
 
 import App from './App';
+
+AppRegistry.registerComponent('MaMeteo', () => App);
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
